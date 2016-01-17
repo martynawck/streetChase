fce879853ec7014d4893ba8941e97ef66909f899
Ania Czajka <Ania Czajka>
2016-01-17 15:59:21 +0100

    costam wyswietla

--- src/main/webapp/resources/js/pages/streetGames.js
+++ src/main/webapp/resources/js/pages/streetGames.js
@@ -1,31 +1,10 @@
 function contactsController($scope, $http) {
-    $scope.pageToGet = 0;
 
-    $scope.state = 'busy';
+    $scope.url = "/streetChase/protected/streetGames/";
 
-    $scope.lastAction = '';
-
-    $scope.url = "/streetChase/protected/contacts/";
-
-    $scope.errorOnSubmit = false;
-    $scope.errorIllegalAccess = false;
-    $scope.displayMessageToUser = false;
-    $scope.displayValidationError = false;
-    $scope.displaySearchMessage = false;
-    $scope.displaySearchButton = false;
-    $scope.displayCreateContactButton = false;
-
-    $scope.contact = {}
-
-    $scope.searchFor = ""
-
-    $scope.getContactList = function () {
+    $scope.getGamesList = function () {
         var url = $scope.url;
-        $scope.lastAction = 'list';
-
         $scope.startDialogAjaxRequest();
-
-        var config = {params: {page: $scope.pageToGet}};
 
         $http.get(url, config)
             .success(function (data) {
@@ -38,46 +17,17 @@ function contactsController($scope, $http) {
     }
 
     $scope.populateTable = function (data) {
-        if (data.pagesCount > 0) {
+        if (data.size() > 0) {
             $scope.state = 'list';
 
-            $scope.page = {source: data.contacts, currentPage: $scope.pageToGet, pagesCount: data.pagesCount, totalContacts : data.totalContacts};
-
-            if($scope.page.pagesCount <= $scope.page.currentPage){
-                $scope.pageToGet = $scope.page.pagesCount - 1;
-                $scope.page.currentPage = $scope.page.pagesCount - 1;
-            }
+            $scope.page = {source: data};
 
             $scope.displayCreateContactButton = true;
-            $scope.displaySearchButton = true;
         } else {
             $scope.state = 'noresult';
             $scope.displayCreateContactButton = true;
-
-            if(!$scope.searchFor){
-                $scope.displaySearchButton = false;
-            }
-        }
-
-        if (data.actionMessage || data.searchMessage) {
-            $scope.displayMessageToUser = $scope.lastAction != 'search';
-
-            $scope.page.actionMessage = data.actionMessage;
-            $scope.page.searchMessage = data.searchMessage;
-        } else {
-            $scope.displayMessageToUser = false;
         }
     }
-
-    $scope.changePage = function (page) {
-        $scope.pageToGet = page;
-
-        if($scope.searchFor){
-            $scope.searchContact($scope.searchFor, true);
-        } else{
-            $scope.getContactList();
-        }
-    };
 
     $scope.exit = function (modalId) {
         $(modalId).modal('hide');
